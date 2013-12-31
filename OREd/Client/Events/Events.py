03818350aa8054a75600a998c01096117e902067
Xeom <fwharf.fw@googlemail.com>
2013-12-31 20:14:29 +0000

    Added new events

--- OREd/Client/Events/Events.py
+++ OREd/Client/Events/Events.py
@@ -1,35 +1,47 @@
 #Base class for an event
 class Event(object):
-	def __init__(self, string):
-		self.string = string
+	def __init__(self, description):
+		self.string = description
 
 	def __repr__(self):
 		return "Event: "+self.string
 
 #An event pertaining to a player
 class PlayerEvent(Event):
-	def __init__(self, player, description=None):
+	def __init__(self, player, description):
 		self.player = player
 		self.string = description
 
 	def GetPlayer(self):
 		pass
+
+#An event relating to the server
+class ServerEvent(Event):
+	pass
 #A player joined the game
-class JoinEvent(PlayerEvent):	
+class Join(PlayerEvent):	
 	pass
 
 #A player is no longer in the game
-class QuitEvent(PlayerEvent):
+class Leave(PlayerEvent):
 	pass
 
 #A player left of their own free will
-class LeaveEvent(QuitEvent):
+class Quit(Leave):
 	pass
 
 #A player was kicked
-class KickEvent(QuitEvent):
+class Kick(Quit):
 	pass
 
 #A player said something
-class TalkEvent(PlayerEvent):
+class Talk(PlayerEvent):
+	pass
+
+#Server started
+class Stop(ServerEvent):
+	pass
+
+#Server stopped
+class Start(ServerEvent):
 	pass
