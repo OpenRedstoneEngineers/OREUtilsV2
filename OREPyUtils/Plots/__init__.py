33b9d320fbcb1f8f03bba3abcb5f28300017785a
Xeom <fwharf.fw@googlemail.com>
2014-07-26 19:27:58 +0100

    Fuck sake, mort

--- OREPyUtils/Plots/__init__.py
+++ OREPyUtils/Plots/__init__.py
@@ -1,9 +1,14 @@
 import Manager
 import Map
 import Frontend
+Map.Config      = None
+Manager.Config  = None
 Frontend.Config = None
 
+
 def OnEnable(**kwargs):
+	Map.Config      = kwargs["conf"]
+	Manager.Config  = kwargs["conf"]
 	Frontend.Config = kwargs["conf"]
 	Frontend.InitManagers()
 
