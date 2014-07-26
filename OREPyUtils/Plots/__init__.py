8e8987caa6a00606341c051eb50425c13244019e
Xeom <fwharf.fw@googlemail.com>
2014-07-26 19:22:18 +0100

    Added Config as an attribute of Frontend

--- OREPyUtils/Plots/__init__.py
+++ OREPyUtils/Plots/__init__.py
@@ -1,8 +1,10 @@
 import Manager
 import Map
 import Frontend
+Frontend.Config = None
 
 def OnEnable(**kwargs):
+	Frontend.Config = kwargs["conf"]
 	Frontend.InitManagers()
 
 def OnDisable(**kwargs):
